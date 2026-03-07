26d9fee3347e380acf5415172714b9fbaf5b0434
rodingoma309-cell <rodingoma309@gmail.com>
2026-03-07 20:11:39 +0100

    Update mangas.js

--- mangas.js
+++ mangas.js
@@ -13,7 +13,7 @@ const mangasDatabase = [
     fullStory:
       "Kafka Hibino et Mina Ashiro se sont promis dans leur enfance de rejoindre la Force de Défense contre les Kaiju. Alors que Mina est devenue capitaine et une héroïne nationale, Kafka a échoué aux examens et nettoie les restes de Kaiju. À 32 ans, il rencontre Reno Ichikawa, un jeune déterminé qui veut aussi rejoindre la Force. Lors d'une mission de nettoyage, un petit Kaiju parasite s'introduit dans le corps de Kafka et lui donne la capacité de se transformer en Kaiju No. 8. Kafka doit maintenant cacher son secret tout en utilisant ses nouveaux pouvoirs pour protéger les autres et réaliser enfin son rêve. Il rejoint la Force de Défense et affronte des Kaiju de plus en plus puissants, tout en évitant d'être découvert.",
     readLink: "https://www.mangaread.org/manga/kaiju-no-8/",
-    image: "https://cdn.myanimelist.net/images/manga/3/238006.jpg",
+    image: "kaiju.jpeg",
     seasons: "Saison 1 (2024) - En cours",
     rating: 4.8,
   },
@@ -28,7 +28,7 @@ const mangasDatabase = [
     fullStory:
       "L'histoire commence avec l'exécution de Gol D. Roger, le Roi des Pirates, qui révèle l'existence du One Piece avant de mourir. Vingt-deux ans plus tard, Monkey D. Luffy, un garçon qui a mangé le Fruit du Démon Gomu Gomu, part à l'aventure pour devenir le prochain Roi des Pirates. Il recrute progressivement son équipage et traverse les mers, affrontant des ennemis redoutables comme Crocodile, Enel, Rob Lucci et les Quatre Empereurs. L'équipage explore les îles mystérieuses, découvre l'histoire du Siècle Perdu, affronte le Gouvernement Mondial et participe à la guerre de Marineford. L'histoire explore les thèmes de l'amitié, de la liberté, de la justice et de la poursuite des rêves.",
     readLink: "https://www.mangaread.org/manga/one-piece/",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "one.jpg",
     seasons: "Saisons 1-20+ (1999-présent)",
     rating: 4.9,
   },
@@ -43,7 +43,7 @@ const mangasDatabase = [
     fullStory:
       "Naruto Uzumaki est un orphelin qui porte en lui le démon renard à neuf queues (Kyubi), scellé en lui par le Quatrième Hokage lors de l'attaque du village. Rejeté par les villageois, il grandit seul et développe un rêve : devenir Hokage pour être reconnu. À l'Académie des Ninjas, il rencontre Sasuke Uchiha, un génie, et Sakura Haruno, dont il est amoureux. Formant l'Équipe 7 avec Kakashi Hatake comme sensei, ils accomplissent des missions et affrontent des ennemis redoutables comme Zabuza, Orochimaru et l'organisation Akatsuki. Après la formation avec Jiraiya, Naruto apprend le Rasengan et maîtrise le mode Sage. Plus tard, il découvre qu'il est la réincarnation d'Asura et doit affronter Sasuke, réincarnation d'Indra. L'histoire culmine avec la Quatrième Grande Guerre Ninja où Naruto et ses alliés combattent Obito, Madara et Kaguya. Finalement, Naruto réalise son rêve et devient le Septième Hokage.",
     readLink: "https://www.mangaread.org/manga/naruto/",
-    image: "naeuto.jpg",
+    image: "naruto.jpg",
     seasons: "Naruto (2002-2007), Shippuden (2007-2017), Boruto (2017-présent)",
     rating: 4.7,
   },
@@ -58,7 +58,7 @@ const mangasDatabase = [
     fullStory:
       "Il y a plus de 100 ans, l'humanité a été presque anéantie par les Titans, des créatures géantes qui dévorent les humains. Les survivants se sont retranchés derrière trois murs concentriques : Maria, Rose et Sina. Eren Yeager, un jeune garçon, rêve de rejoindre le Bataillon d'Exploration pour découvrir le monde extérieur. Lorsque le Mur Maria est détruit par un Titan Colossal, sa mère est dévorée. Eren, sa sœur adoptive Mikasa Ackerman et leur ami Armin Arlert rejoignent le Bataillon d'Exploration. Eren découvre qu'il peut se transformer en Titan et utilise ce pouvoir pour combattre les Titans. L'histoire révèle progressivement que les Titans sont en fait des humains transformés, et que le vrai ennemi est le royaume de Marley qui utilise les Titans comme armes. Eren déclenche le Rumbling, libérant des millions de Titans pour détruire le monde extérieur. L'histoire explore des thèmes de liberté, de vengeance, de cycle de la haine et de la complexité morale.",
     readLink: "https://www.mangaread.org/manga/attack-on-titan/",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "attack.jpg",
     seasons: "Saisons 1-4 (2013-2023) - Terminé",
     rating: 4.9,
   },
@@ -73,7 +73,7 @@ const mangasDatabase = [
     fullStory:
       "Tanjiro Kamado vit avec sa famille dans les montagnes. Un jour, en rentrant de la ville, il découvre sa famille massacrée par des démons, sauf sa sœur Nezuko qui a été transformée en démon. Nezuko conserve cependant une partie de son humanité et protège Tanjiro. Tanjiro rencontre Giyu Tomioka, un Pilleur de Démons, qui lui suggère de trouver Sakonji Urokodaki pour devenir un Pilleur. Tanjiro s'entraîne pendant deux ans et passe la sélection finale. Il rejoint les Pilleurs de Démons et commence à chasser des démons tout en cherchant un moyen de rendre Nezuko humaine. Il rencontre Zenitsu Agatsuma et Inosuke Hashibira qui deviennent ses compagnons. Ensemble, ils affrontent les Douze Lunes Démoniaques, les serviteurs les plus puissants de Muzan Kibutsuji, le premier démon. L'histoire culmine avec la bataille finale contre Muzan et la transformation de Nezuko en humaine.",
     readLink: "https://www.mangaread.org/manga/demon-slayer/",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "demon.jpg",
     seasons: "Saisons 1-3 (2019-2023) - Terminé",
     rating: 4.8,
   },
@@ -88,7 +88,7 @@ const mangasDatabase = [
     fullStory:
       "Dans un monde où la plupart des gens possèdent des super-pouvoirs appelés 'Alters', Izuku Midoriya est né sans pouvoir. Malgré cela, il rêve de devenir un héros comme son idole, All Might, le plus grand héros. Un jour, All Might lui transmet son Alter 'One For All', un pouvoir qui s'accumule de génération en génération. Izuku entre au lycée Yuei, une école pour héros, où il apprend à maîtriser son pouvoir avec l'aide de ses professeurs et de ses camarades de classe, notamment Katsuki Bakugo, son rival d'enfance, et Ochaco Uraraka. Il affronte des super-vilains comme la Ligue des Vilains et Tomura Shigaraki, tout en découvrant les secrets d'One For All et de son ennemi, All For One. L'histoire explore la croissance d'Izuku, ses relations avec ses camarades, et sa quête pour devenir le plus grand héros.",
     readLink: "https://www.mangaread.org/manga/my-hero-academia/",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "hero.jpeg",
     seasons: "Saisons 1-6 (2016-présent)",
     rating: 4.7,
   },
@@ -103,7 +103,7 @@ const mangasDatabase = [
     fullStory:
       "Yuji Itadori est un lycéen exceptionnellement fort qui rejoint le club d'occultisme de son école. Un jour, un objet scellé, un doigt de Ryomen Sukuna, le Roi des Fléaux, est ouvert. Yuji avale le doigt pour protéger ses amis et devient l'hôte de Sukuna. Au lieu d'être exécuté, Satoru Gojo, le plus puissant sorcier de jujutsu, lui propose de rejoindre le Lycée Technique de Tokyo pour apprendre à contrôler Sukuna. Yuji s'entraîne avec ses camarades Nobara Kugisaki et Megumi Fushiguro sous la direction de leurs professeurs. Il affronte des fléaux et des sorciers maléfiques tout en apprenant à maîtriser l'énergie maudite. L'histoire explore les thèmes de la mort, de la moralité et de la lutte contre les forces du mal dans un monde où les émotions négatives créent des créatures maléfiques.",
     readLink: "https://www.mangaread.org/manga/jujutsu-kaisen/",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "kaisen.jpg",
     seasons: "Saisons 1-2 (2020-présent)",
     rating: 4.8,
   },
@@ -118,7 +118,7 @@ const mangasDatabase = [
     fullStory:
       "Denji est un jeune homme pauvre qui vit avec Pochita, un chien-démon Chainsaw. Il travaille pour les yakuzas pour rembourser les dettes de son père. Après avoir été trahi et tué, Pochita fusionne avec son cœur et le ressuscite. Denji devient Chainsaw Man, capable de transformer ses membres en tronçonneuses. Il rejoint la Division de Sécurité Publique et travaille avec d'autres chasseurs de démons comme Power et Aki. L'histoire explore des thèmes de survie, de désir et de manipulation dans un monde où les démons sont créés par les peurs humaines. Denji doit naviguer entre ses désirs simples et les complots complexes qui l'entourent.",
     readLink: "https://www.mangaread.org/manga/chainsaw-man/",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "man.jpeg",
     seasons: "Saison 1 (2022) - En cours",
     rating: 4.9,
   },
@@ -133,7 +133,7 @@ const mangasDatabase = [
     fullStory:
       "Loid Forger, alias Twilight, est le meilleur espion du WISE. Sa mission : s'infiltrer dans une école d'élite pour se rapprocher de Donovan Desmond, un homme politique dangereux. Pour cela, il doit créer une famille de façade. Il adopte Anya, une petite fille télépathe qui lit dans les pensées, et épouse Yor Briar, une tueuse à gages professionnelle qui se fait passer pour une fonctionnaire. Chacun cache son identité aux autres, créant une famille dysfonctionnelle mais attachante. L'histoire suit leurs aventures comiques et parfois dangereuses alors qu'ils tentent de maintenir leur façade tout en développant de vrais liens familiaux.",
     readLink: "https://www.mangaread.org/manga/spy-x-family/",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "spy.jpeg",
     seasons: "Saisons 1-2 (2022-présent)",
     rating: 4.8,
   },
@@ -145,7 +145,7 @@ const mangasDatabase = [
     category: "action",
     description:
       "Ken Kaneki devient un demi-ghoul après une greffe d'organe et doit apprendre à vivre entre deux mondes.",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "tokyo.jpeg",
     seasons: "Saisons 1-4 (2014-2018) - Terminé",
     rating: 4.6,
   },
@@ -157,7 +157,7 @@ const mangasDatabase = [
     category: "action",
     description:
       "Light Yagami trouve un carnet qui permet de tuer quiconque en écrivant son nom, et décide de créer un monde parfait.",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "death.jpg",
     seasons: "Saison 1 (2006-2007) - Terminé",
     rating: 4.9,
   },
@@ -169,7 +169,7 @@ const mangasDatabase = [
     category: "action",
     description:
       "Les frères Elric utilisent l'alchimie pour retrouver leurs corps après une tentative de résurrection ratée.",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "full.jpeg",
     seasons: "Brotherhood (2009-2010) - Terminé",
     rating: 4.9,
   },
@@ -181,7 +181,7 @@ const mangasDatabase = [
     category: "action",
     description:
       "Les aventures de Goku et ses amis qui défendent la Terre contre des ennemis de plus en plus puissants.",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "dragon.jpeg",
     seasons: "Saisons 1-9 (1989-1996) - Terminé",
     rating: 4.7,
   },
@@ -205,7 +205,7 @@ const mangasDatabase = [
     category: "romance",
     description:
       "Tohru Honda découvre que la famille Sohma est maudite par les animaux du zodiaque chinois.",
-    image: "https://cdn.myanimelist.net/images/manga/2/253146.jpg",
+    image: "basket.jpeg",
     seasons: "Saisons 1-3 (2019-2021) - Terminé",
     rating: 4.8,
   },
@@ -293,6 +293,8 @@ const mangaImageUrls = [
   "https://cdn.myanimelist.net/images/manga/2/253146.jpg", // One Punch Man
   "https://cdn.myanimelist.net/images/manga/2/253146.jpg", // Mob Psycho
   "https://cdn.myanimelist.net/images/manga/2/253146.jpg", // Promised Neverland
+  "pluto.jpeg", // Pluto
+  "Monster.jpeg", // Monster
 ];
 
 // Générer plus de mangas pour atteindre 100-5000
